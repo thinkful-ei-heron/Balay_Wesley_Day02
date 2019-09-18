d60ec799746f831787984e317e480a3da411f7bc
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-18 09:06:37 -0700

    first drill complete

---
+++ js.js
@@ -0,0 +1,12 @@
+'use strict';
+function createMyObject(){
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      console.log('hello');
+      return 'hello';
+    }
+  };
+}
