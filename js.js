510e65382b8e21921e96f1ba0e7d4fe610a0938c
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-18 11:14:09 -0700

    findById

--- js.js
+++ js.js
@@ -67,5 +67,56 @@ function enrollInSummerSchool(students) {
 }
 */
 
+// you can pass in `scratchData` to test out `findByid`
+// your function
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
 
+function findById(items, idNum) {
+  // your code here
+  let result = {};
+  for ( let i = 0; i < items.length; i++){
+    if(Object.values(items[i]).includes(idNum)){
+      result = items[i];
+    }
+  }
+  return result;
+}
+
+findById(scratchData,3);
+
+
+
+//
+
+function testIt() {
+  const testData = [
+    { id: 1, foo: 'bar' },
+    { id: 2, foo: 'bizz' },
+    { id: 3, bang: 'boo' },
+  ];
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return;
+  }
+  if (result.id !== 3) {
+    console.error(
+      'Asked for item with id of `3` but got back one with id of ' + result.id
+    );
+    return;
+  }
+  if (result.bang !== 'boo') {
+    console.error(
+      'Expected all key/value pairs from target object to be returned'
+    );
+    return;
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
 
