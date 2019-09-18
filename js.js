1bd17664b8f8fb7c6e1691a193ff074925f1416a
balayaydemir <balayaydemir@gmail.com>
2019-09-18 10:56:20 -0700

    summerschool_complete

--- js.js
+++ js.js
@@ -46,63 +46,26 @@ function personMaker() {
 }
 */
 
+/*
 function makeStudentsReport(data) {
-  // your code here
-  let studentReport = [];
-
-  Object.keys(data).forEach(function(key) {
-    console.log(`${key},  ${data[key]}`);
-  return studentReport;
+  let finalReport = [];
+  let studentReports = data.forEach(student => {
+    finalReport.push(`${student.name}: ${student.grade}`);
   });
-
-/* From here down, you are not expected to 
-   understand.... for now :)  
-   
-   Nothing to see here!
-   
+  return finalReport;
+}
 */
 
-// tests
-
-function testIt() {
-  const testData = [
-    { name: 'Jane Doe', grade: 'A' },
-    { name: 'John Dough', grade: 'B' },
-    { name: 'Jill Do', grade: 'A' },
-  ];
-
-  const expectations = ['Jane Doe: A', 'John Dough: B', 'Jill Do: A'];
-
-  const results = makeStudentsReport(testData);
-
-  if (!(results && results instanceof Array)) {
-    console.error('FAILURE: `makeStudentsReport` must return an array');
-    return;
+/*
+function enrollInSummerSchool(students) {
+  let newArray = [];
+  for (let i = 0; i < students.length; i++) {
+    students[i].status = 'In summer school';
+    newArray.push(students[i]);
   }
-  if (results.length !== testData.length) {
-    console.error(
-      'FAILURE: test data had length of ' +
-        testData.length +
-        ' but `makeStudentsReport` returned array of length ' +
-        results.length
-    );
-    return;
-  }
-  for (let i = 0; i < expectations.length; i++) {
-    let expect = expectations[i];
-    if (
-      !results.find(function(item) {
-        return item === expect;
-      })
-    ) {
-      console.error(
-        'FAILURE: `makeStudentsReport` is not ' + 'producing expected strings'
-      );
-      return;
-    }
-  }
-  console.log('SUCCESS: `makeStudentsReport` is working');
+  return newArray;
 }
+*/
 
-testIt();
+
 
