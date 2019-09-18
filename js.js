c020a925c63d09972c196866477a7e3a4f232acd
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-09-18 10:45:05 -0700

    first attempt student report

--- js.js
+++ js.js
@@ -39,9 +39,70 @@ function personMaker() {
 */
 
 
-function keyDeleter(obj) {
+/*function keyDeleter(obj) {
   delete obj.foo;
   delete obj.bar;
   return obj;
 }
+*/
+
+function makeStudentsReport(data) {
+  // your code here
+  let studentReport = [];
+
+  Object.keys(data).forEach(function(key) {
+    console.log(`${key},  ${data[key]}`);
+  return studentReport;
+  });
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   Nothing to see here!
+   
+*/
+
+// tests
+
+function testIt() {
+  const testData = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
+  ];
+
+  const expectations = ['Jane Doe: A', 'John Dough: B', 'Jill Do: A'];
+
+  const results = makeStudentsReport(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `makeStudentsReport` must return an array');
+    return;
+  }
+  if (results.length !== testData.length) {
+    console.error(
+      'FAILURE: test data had length of ' +
+        testData.length +
+        ' but `makeStudentsReport` returned array of length ' +
+        results.length
+    );
+    return;
+  }
+  for (let i = 0; i < expectations.length; i++) {
+    let expect = expectations[i];
+    if (
+      !results.find(function(item) {
+        return item === expect;
+      })
+    ) {
+      console.error(
+        'FAILURE: `makeStudentsReport` is not ' + 'producing expected strings'
+      );
+      return;
+    }
+  }
+  console.log('SUCCESS: `makeStudentsReport` is working');
+}
+
+testIt();
 
